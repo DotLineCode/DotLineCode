c1c0bcf74bd1bb83d9bb0f415ef89a26ce0a3cb6
DotLineCode <kehnyth05val@gmail.com>
2025-01-12 19:00:24 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import argparse
 from src.art_generator import ArtGenerator
 from src.utils import display_image
 
-def main():
+def main(): 
     parser = argparse.ArgumentParser(description="Lee AI Art Generator")
     parser.add_argument('--prompt', type=str, required=True, help='Art description prompt')
     args = parser.parse_args()
